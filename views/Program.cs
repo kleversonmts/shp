67ad3348f79b6714a3e22556b58f8fc94213f714
Kleverson Matos <kleversonmts@gmail.com>
2019-01-15 11:10:27 -0200

    Clean

--- views/Program.cs
+++ views/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace shp.site
+namespace views
 {
     public class Program
     {
